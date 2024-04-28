695118cbf150df24f2c86fab04dd257955e7b763
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2024-04-28 20:22:31 +0200

    Updated NeoForge, moved displaytest to mods.toml

--- src/main/java/redstonedubstep/mods/vanishmod/Vanishmod.java
+++ src/main/java/redstonedubstep/mods/vanishmod/Vanishmod.java
@@ -1,19 +1,16 @@
 package redstonedubstep.mods.vanishmod;
 
-import net.neoforged.fml.IExtensionPoint;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
-import net.neoforged.neoforge.network.NetworkConstants;
 
 @Mod(Vanishmod.MODID)
 public class Vanishmod {
 	public static final String MODID = "vmod"; //This is Vanishmod v1.1.15 for 1.20.4!
 
 	public Vanishmod() {
-		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		NeoForge.EVENT_BUS.addListener(this::registerCommands);
 		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, VanishConfig.SERVER_SPEC, "vanishmod-server.toml");
 	}
