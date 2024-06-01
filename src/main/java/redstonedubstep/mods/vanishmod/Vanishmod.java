89e639fd87d84195a8a0033cd71056d397fd950b
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2024-06-01 18:07:44 +0200

    v1.1.16

--- src/main/java/redstonedubstep/mods/vanishmod/Vanishmod.java
+++ src/main/java/redstonedubstep/mods/vanishmod/Vanishmod.java
@@ -8,7 +8,7 @@ import net.neoforged.neoforge.event.RegisterCommandsEvent;
 
 @Mod(Vanishmod.MODID)
 public class Vanishmod {
-	public static final String MODID = "vmod"; //This is Vanishmod v1.1.15 for 1.20.4!
+	public static final String MODID = "vmod"; //This is Vanishmod v1.1.16 for 1.20.4!
 	public static boolean mc2discordDetected = false;
 
 	public Vanishmod() {
